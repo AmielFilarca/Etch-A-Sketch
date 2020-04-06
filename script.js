adc9222a74f9b71775f0a368834c3497eb9531f7
Amiel Filarca <amielfilarca@gmail.com>
2020-04-06 12:39:44 +0800

    Added workaround for a bug

--- script.js
+++ script.js
@@ -44,7 +44,9 @@ function randomColorGrid() {
                 e.target.style.backgroundColor = color;
                 gridContainer.style.boxShadow = '0 0 30px ' + color;
             } else {
-                e.target.style.backgroundColor = darkerShade(e.target.style.backgroundColor);
+                let color = darkerShade(e.target.style.backgroundColor)
+                e.target.style.backgroundColor = color;
+                gridContainer.style.boxShadow = '0 0 30px ' + color;
             }
         });
     });
@@ -54,7 +56,6 @@ function blackColorGrid() {
     const cells = document.querySelectorAll('.cell');
     cells.forEach((cell) => {
         cell.addEventListener('mouseover', (e) => {
-            e.target.classList.remove(".colored");
             e.target.style.backgroundColor = '#494949';
             gridContainer.style.boxShadow = '0 0 30px #494949';
         });
@@ -86,6 +87,8 @@ resizeButton.addEventListener('click', (e) => {
 })
 
 rgbToggle.addEventListener('click', (e) => {
+    resetGrid()
+    createGrid(gridSize)
     if (checkToggle()) {
         randomColorGrid()
     } else {
